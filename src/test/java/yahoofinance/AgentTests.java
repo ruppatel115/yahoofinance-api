b78d514e9940f05856e61b2689466570ffb2de11
Mike Santo <aero10599@gmail.com>
2021-04-22 10:48:28 -0400

    buy tests written, failing

--- src/test/java/yahoofinance/AgentTests.java
+++ src/test/java/yahoofinance/AgentTests.java
@@ -15,7 +15,7 @@ import yahoofinance.histquotes.HistoricalQuote;
 public class AgentTests {
     
     @Test
-    public static void buyTests() throws IOException{
+    public void buyTests() throws IOException{
         Simulator simulator = new Simulator();
         String[] symbols = new String[] {"INTC", "BABA", "TSLA", "GOOG"};
         List<Stock>stockList = simulator.getStockInfo(symbols);
@@ -25,7 +25,33 @@ public class AgentTests {
         //checking portfolio
         assertEquals(1000000, simulator.getPortfolio().getBuyingPower(), .001);
 
+        //gets current price of stock
+        double babaPrice = stockList.get(1).getQuote().getPrice().doubleValue();
+
+        //how much money can be spent on one stock
+        double currMoney = simulator.getPortfolio().getBuyingPower()*.10;
+
+        //how many shares should be purchased
+        int shares = (int) (currMoney/babaPrice);
+
+        //stock object
+        Stock baba = YahooFinance.get("BABA");
+
+        //buying stock
+        agent.buyStock(simulator.getSensor(), "BABA");        
+
+        //checks that a stock was purchased
+        assertEquals(1, simulator.getPortfolio().getPortfolio().size());
+
+        //checks that it was purchased at the current price
+        assertEquals(babaPrice, simulator.getPortfolio().getPriceBoughtAt().get(baba), .001);
+
+        //checks that the right amount was purchased
+        assertEquals(shares, (int) simulator.getPortfolio().getPortfolio().get(baba));
+
         
+
+
 
     }
 
